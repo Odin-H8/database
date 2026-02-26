3ae6b188b9d41dab13bde5d6330c84db35e41b82
Johan Martin Skavhaug <j.skavhaug@sportradar.com>
2026-02-26 18:33:03 +0100

    Add description

--- migrations/00090_add_randomizer_gamemode.sql
+++ migrations/00090_add_randomizer_gamemode.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-INSERT INTO match_type (id, name) VALUES (18, 'Randomizer');
+INSERT INTO match_type (id, name, description) VALUES (18, 'Randomizer', 'Random!');
 
 ALTER TABLE `matches`
     ADD COLUMN `seed` VARCHAR(255) NULL;
